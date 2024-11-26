067b7070981663d94b926038846790d92097370e
Neerajeyy <sanilknrsanilknr@gmail.com>
2024-11-26 12:26:40 +0530

    Add files via upload

---
+++ ARRAYS.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a=1,b=2,c=3;
+clrscr();
+printf("a=%d\nb=%d\nc=%d\n",a,b,c);
+printf("adrress of a =%ld \nadrress of b =%ld \nadrress of=%ld \n",&a,&b,&c);
+}
+
